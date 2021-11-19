da0ff08fd071aae0c65044e99d2b4e5e355467ad
yukti <yukti.8826@gmail.com>
2021-11-20 00:55:39 +0530

    demo

---
+++ src/com/coding/leetcode/editor/en/[567]Permutation in String.java
@@ -0,0 +1,40 @@
+//Given two strings s1 and s2, return true if s2 contains a permutation of s1, 
+//or false otherwise. 
+//
+// In other words, return true if one of s1's permutations is the substring of 
+//s2. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s1 = "ab", s2 = "eidbaooo"
+//Output: true
+//Explanation: s2 contains one permutation of s1 ("ba").
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s1 = "ab", s2 = "eidboaoo"
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s1.length, s2.length <= 10⁴ 
+// s1 and s2 consist of lowercase English letters. 
+// 
+// Related Topics Hash Table Two Pointers String Sliding Window 👍 3636 👎 97
+
+package com.coding;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    System.out.println("hello this is demo");
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
